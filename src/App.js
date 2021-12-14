3fb4caa9c0fe20363c156a21b9161c3499f45e79
Peter Conley <peterdavidconley@gmail.com>
2021-12-14 14:36:34 -0500

    created header and simple login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Swtich} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <div className='heading'>
+        <h3>Friends Database</h3>
+        <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/friends-list">Friends List</Link>
+            </li>
+            <li>
+              <Link to="/add-friend">Add Friend</Link>
+            </li>
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+        </ul>    
+      </div>
+      <div className='login-form'>
+        <h2>LOGIN</h2>
+        <form>
+          <label>USERNAME:
+            <input 
+            type='text'
+            placeholder='enter your username'
+            />
+          </label>
+          <label>PASSWORD:
+            <input 
+            type='text'
+            placeholder='enter your password'
+            />
+          </label>
+          <button>SUBMIT</button>
+        </form>
+      </div>
       <Switch>
         <Route path="/login" />
         <Route path="/" />
@@ -13,5 +47,5 @@ function App() {
     </div>
   );
 }
-s
+
 export default App;
